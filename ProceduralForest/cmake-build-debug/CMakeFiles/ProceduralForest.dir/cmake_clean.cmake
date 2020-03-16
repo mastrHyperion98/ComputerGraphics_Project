3ff0bdda0282db585c3164eebffef6cb246f4f55
mastrHyperion98 <s_smith1998@outlook.com>
2020-03-16 16:06:15 -0400

    Initial Commit

---
+++ ProceduralForest/cmake-build-debug/CMakeFiles/ProceduralForest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProceduralForest.dir/main.cpp.o"
+  "ProceduralForest"
+  "ProceduralForest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProceduralForest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
