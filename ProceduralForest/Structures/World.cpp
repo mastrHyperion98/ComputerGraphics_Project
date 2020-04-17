bef9cfc68aee8801997a2972d492954f20f73db9
mastrHyperion98 <s_smith1998@outlook.com>
2020-04-16 21:38:10 -0400

    Update World and Cmake

--- ProceduralForest/Structures/World.cpp
+++ ProceduralForest/Structures/World.cpp
@@ -61,39 +61,5 @@ void World::Update() {
 }
 
 void World::ProcedurallyGenerateWorld() {
-    // create a basic terrain
-    Material simple_ground_material;
-    simple_ground_material.addTexture("../Assets/Textures/grass3.bmp");
-    // 1500 * 1500 terrain
-    Entity *terrain = new Entity;
-    for(int i = 0; i <10;i++){
-        for(int j = 0; j < 10; j++){
-            Cube *cube = new Cube(simple_ground_material, vec3(100,0.1,100));
-            cube->getTransform().position = (vec3(100*i ,0,-100*j ));
-            terrain->addComponent(cube);
-        }
-    }
-    for(int i = 0; i <10;i++){
-        for(int j = 0; j < 10; j++){
-            Cube *cube = new Cube(simple_ground_material, vec3(100,0.1,100));
-            cube->getTransform().position = (vec3(100*i ,0,100*j ));
-            terrain->addComponent(cube);
-        }
-    }
 
-    AddEntities(terrain);
-    // several options --> instancing or drawing ~10 cubes and allow for different textures.
-    // generate random spacing
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            AddEntities(TreeGenerator::generateTree(vec3{-30 + i*15,0,-j*15}));
-        }
-    }
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            AddEntities(TreeGenerator::generateTree(vec3{-30 + i*15,0,j*15}));
-        }
-    }/*
-    Terrain *terrain = new Terrain();
-    AddEntities(terrain);*/
 }
