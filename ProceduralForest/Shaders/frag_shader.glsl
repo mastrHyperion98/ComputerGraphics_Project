18be631ec174e523deb7eaa12328f383d6445f38
mastrHyperion98 <s_smith1998@outlook.com>
2020-04-18 16:15:39 -0400

    Update vertex and fragment shader to now work using OpenGL 3.3+

--- ProceduralForest/Shaders/frag_shader.glsl
+++ ProceduralForest/Shaders/frag_shader.glsl
@@ -1,4 +1,4 @@
-#version 430 core
+#version 330 core
 
 struct Material{
     vec3 color;
@@ -12,14 +12,19 @@ in vec2 vertexUV;
 in vec3 Normal;
 in float TextID;
 out vec4 FragColor;
-
+const int tex0 = 0;
+const int tex1 = 1;
 uniform Material material;
 uniform bool enabled_textures = true;
 
 void main()
 {
+    vec4 textureColor;
     int id = int(TextID);
-    vec4 textureColor = texture(material.diffuseMap[id], vertexUV);
+    if(id == 0)
+        textureColor = texture(material.diffuseMap[tex0], vertexUV);
+    else
+        textureColor = texture(material.diffuseMap[tex1], vertexUV);
    if(!material.isTextured || !enabled_textures )
         FragColor =  vec4(material.color, 1.0);
    else {
